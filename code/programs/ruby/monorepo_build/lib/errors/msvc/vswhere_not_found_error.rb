594bc8d978495ba1e33c3cc699c9087c0c7726cf
Adhithya Rajasekaran <adhithyan15@gmail.com>
2025-05-06 00:16:20 -0700

    Attempt to use sbcl from Chocolatey

--- code/programs/ruby/monorepo_build/lib/errors/msvc/vswhere_not_found_error.rb
+++ code/programs/ruby/monorepo_build/lib/errors/msvc/vswhere_not_found_error.rb
@@ -1,4 +1,4 @@
-class VswhereNotFoundError < StandardError
+class VSWhereNotFoundError < StandardError
   def initialize
     super('vswhere.exe not found at standard location or in PATH. Cannot locate Visual Studio.')
   end
